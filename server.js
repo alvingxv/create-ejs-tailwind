4f9f6cf2f8474364f8a3c299dff366f78979918d
Alvian Ghifari <alvian2002286@gmail.com>
2022-07-10 22:28:19 +0700

    add controller and routes

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const app = express();
-
+const express = require('express')
+const app = express()
 const path = require('path')
+const PORT = process.env.PORT || 3000
+const { router } = require('./routes/routes')
 
-const PORT = process.env.PORT || 3000;
 
+//setup view engine using ejs and tailwind
 app.set('views', path.join(__dirname, './views/'))
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 app.use("/public", express.static('public'))
 
+//routes
+app.use('/', router)
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
+//listen to port
 app.listen(PORT, () => {
     console.log(`server is listening on ${PORT},
-    http://localhost:${PORT}`);
-});
+    http://localhost:${PORT}`)
+})
